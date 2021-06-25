a091454084ff702e649d0a6cf6b28a05fd8ecb90
lyric <1353758518@qq.com>
2021-06-25 15:03:55 +0800

    feat: 小程序网络请求 Promise 化

---
+++ src/request.js
@@ -0,0 +1,25 @@
+// 小程序请求
+function request(
+  url = "",
+  data = {},
+  options = {},
+) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url,
+      data,
+      header: options.header,
+      method: options.method,
+      dataType: options.dataType,
+      responseType: options.responseType,
+      success: (res) => {
+        resolve(res);
+      },
+      fail: (err) => {
+        reject(err);
+      },
+    });
+  });
+}
+
+export default request;
