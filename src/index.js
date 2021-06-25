ba72eda0e18d3791d51415252781afd7e84baf91
lyric <1353758518@qq.com>
2021-06-25 15:06:52 +0800

    feat: 基础请求功能实现

--- src/index.js
+++ src/index.js
@@ -1,4 +1,26 @@
+import request from "./request";
+
 class MpRequest {
+
+  options = {
+    header: { "content-type": "application/json" },
+    method: "GET",
+    dataType: "json",
+    responseType: "text",
+  }
+
+  constructor(options) {
+    this.options = Object.assign(this.options, options);
+  }
+
+  request(
+    url = "",
+    params = {},
+    options = {},
+  ) {
+    options = Object.assign(this.options, options);
+    return request(url, params, options);
+  }
 
 }
 
